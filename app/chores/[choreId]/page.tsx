231c43f9efb7b4e2fcc7df1c2ba5722995e28a44
ChrisKnowsDev <chrisharley81@gmail.com>
2025-05-22 13:20:01 -0400

    created placehold component for not found chores

--- app/chores/[choreId]/page.tsx
+++ app/chores/[choreId]/page.tsx
@@ -1,5 +1,9 @@
+import Link from 'next/link';
+
 import { initialChores } from '@/app/utils/data';
+import { choresPath } from '@/app/utils/paths';
 import { Placeholder } from '@/components/placeholder';
+import { Button } from '@/components/ui/button';
 
 type ChoresPageProps = {
   params: Promise<{ choreId: string }>;
@@ -10,7 +14,19 @@ export default async function ChorePage({ params }: ChoresPageProps) {
   const chore = initialChores.find((chore) => chore.id === choreId);
 
   if (!chore) {
-    return <Placeholder label="Chore not found" />;
+    return (
+      <div className="flex flex-1">
+        <Placeholder
+          label="Chore not found"
+          button={
+            <Button asChild variant="outline">
+              <Link href={choresPath()}>Go back to chores</Link>
+            </Button>
+          }
+        />
+        <Placeholder label="Chore not found" />
+      </div>
+    );
   }
   return <div>ChoresPage for {chore?.title}</div>;
 }
--- components/placeholder.tsx
+++ components/placeholder.tsx
@@ -1,11 +1,24 @@
+import { LucideCircleAlert } from 'lucide-react';
+import { cloneElement } from 'react';
+
 type PlaceholderProps = {
   label: string;
+  icon?: React.ReactElement<React.ComponentPropsWithoutRef<'svg'>>;
+  button?: React.ReactNode;
 };
 
-export function Placeholder({ label }: PlaceholderProps) {
+export function Placeholder({
+  label,
+  icon = <LucideCircleAlert />,
+  button = <div className="h-10" />,
+}: PlaceholderProps) {
   return (
-    <div className="flex-1 self-center flex flex-col items-center justify-center">
+    <div className="flex flex-1 self-center flex-col items-center justify-center mt-40">
+      {cloneElement(icon, {
+        className: 'w-8 h-8',
+      })}
       <h2 className="text-lg text-center">{label}</h2>
+      {button}
     </div>
   );
 }
