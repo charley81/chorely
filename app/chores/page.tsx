1dadc6e8f65cc007fec2ebe34607054d11d457b4
ChrisKnowsDev <chrisharley81@gmail.com>
2025-05-28 10:37:49 -0400

    initial data fetch in server component

--- app/chores/page.tsx
+++ app/chores/page.tsx
@@ -1,13 +1,15 @@
-import { initialChores } from '@/app/utils/data';
 import { Heading } from '@/components/heading';
 import { ChoreItem } from '@/features/chore/components/chore-item';
+import { getChores } from '@/features/queries/get-chores';
 
-export default function ChoresPage() {
+export default async function ChoresPage() {
+  const chores = await getChores();
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Chores" description="A place for all your chores" />
+
       <div className="flex flex-1 flex-col items-center gap-y-4 animate-fade-in-from-top">
-        {initialChores.map((chore) => (
+        {chores.map((chore) => (
           <ChoreItem key={chore.id} chore={chore} />
         ))}
       </div>
---
+++ features/queries/get-chores.ts
@@ -0,0 +1,9 @@
+import { initialChores } from '@/app/utils/data';
+import { Chore } from '../chore/types';
+
+export const getChores = async (): Promise<Chore[]> => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return new Promise((resolve) => {
+    resolve(initialChores);
+  });
+};
