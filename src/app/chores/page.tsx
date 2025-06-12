b0dc767986355127326304c080866ae1d8b7293c
ChrisKnowsDev <chrisharley81@gmail.com>
2025-06-12 18:01:42 -0400

    added conditional styling to chore cards

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { LucideBicepsFlexed, LucideCheck, LucideLockOpen } from 'lucide-react';
 import Link from 'next/link';
 
@@ -26,13 +27,25 @@ export default function ChoresPage() {
       <div className="mt-20 flex max-w-2xl flex-col gap-y-4">
         {initialChores.map((chore) => (
           <div key={chore.id} className="rounded border border-slate-100 p-4">
-            <p className="flex items-end gap-x-1">
+            <p
+              className={clsx('flex items-end gap-x-1', {
+                'text-blue-500': chore.status === 'DONE',
+                'text-green-500': chore.status === 'OPEN',
+                'text-red-500': chore.status === 'WORKING',
+              })}
+            >
               {CHORE_ICONS[chore.status].icon}
               <span className="text-xs">{CHORE_ICONS[chore.status].text}</span>
             </p>
             <div className="py-2">
               <h3 className="truncate text-2xl font-bold">{chore.title}</h3>
-              <p className="truncate text-slate-400">{chore.conent}</p>
+              <p
+                className={clsx('truncate text-slate-400', {
+                  'line-through': chore.status === 'DONE',
+                })}
+              >
+                {chore.conent}
+              </p>
             </div>
             <Link href={chorePath(chore.id)} className="underline">
               view
