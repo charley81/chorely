88ec4846469537795588f3266c3a575890ec69dd
ChrisKnowsDev <chrisharley81@gmail.com>
2025-06-12 16:48:26 -0400

    setup chore status with icon and text description

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,7 +1,23 @@
+import { LucideBicepsFlexed, LucideCheck, LucideLockOpen } from 'lucide-react';
 import Link from 'next/link';
 
 import { initialChores } from '@/data';
 import { chorePath } from '@/path';
+
+const CHORE_ICONS = {
+  DONE: {
+    icon: <LucideCheck />,
+    text: 'Done',
+  },
+  OPEN: {
+    icon: <LucideLockOpen />,
+    text: 'Open',
+  },
+  WORKING: {
+    icon: <LucideBicepsFlexed />,
+    text: 'Working',
+  },
+};
 
 export default function ChoresPage() {
   return (
@@ -10,6 +26,10 @@ export default function ChoresPage() {
       <div className="flex max-w-2xl flex-col gap-y-4">
         {initialChores.map((chore) => (
           <div key={chore.id}>
+            <p className="flex items-end gap-x-1">
+              {CHORE_ICONS[chore.status].icon}
+              <span className="text-xs">{CHORE_ICONS[chore.status].text}</span>
+            </p>
             <h3 className="text-2xl font-bold">{chore.title}</h3>
             <Link href={chorePath(chore.id)}>view</Link>
           </div>
--- src/data.ts
+++ src/data.ts
@@ -3,12 +3,18 @@ export const initialChores = [
     id: '1',
     title: 'chore 1',
     conent: 'this is the first chore',
-    status: 'DONE',
+    status: 'DONE' as const,
   },
   {
     id: '2',
     title: 'chore 2',
     conent: 'this is the second chore',
-    status: 'OPEN',
+    status: 'OPEN' as const,
+  },
+  {
+    id: '3',
+    title: 'chore 3',
+    conent: 'this is the third chore',
+    status: 'WORKING' as const,
   },
 ];
