78a4b08e11e666e986c46ae99cbdc85ba49dde02
ChrisKnowsDev <chrisharley81@gmail.com>
2025-07-24 10:11:43 -0400

    add form validation with zod

--- src/features/chores/actions/upsert-chore.ts
+++ src/features/chores/actions/upsert-chore.ts
@@ -1,27 +1,38 @@
 'use server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import * as z from 'zod';
 
 import prisma from '@/lib/prisma';
 import { chorePath, choresPath } from '@/paths';
+
+const upsertChoreScema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertChore = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData,
 ) => {
-  const data = {
-    title: formData.get('title') as string,
-    content: formData.get('content') as string,
-  };
+  try {
+    const data = upsertChoreScema.parse({
+      title: formData.get('title') as string,
+      content: formData.get('content') as string,
+    });
 
-  await prisma.chore.upsert({
-    where: {
-      id: id || '',
-    },
-    update: data,
-    create: data,
-  });
+    await prisma.chore.upsert({
+      where: {
+        id: id || '',
+      },
+      update: data,
+      create: data,
+    });
+  } catch (error) {
+    console.log(error);
+    return { message: 'Something went wrong' };
+  }
 
   revalidatePath(choresPath());
 
