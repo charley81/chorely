7e300c6029a7337975ee34dd2ddab3d4f8fdf7ee
ChrisKnowsDev <chrisharley81@gmail.com>
2025-07-24 07:10:41 -0400

    create dynamic card-compact compoent to wrap forms

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,14 +1,8 @@
 import { Suspense } from 'react';
 
+import { CardCompact } from '@/components/card-compact';
 import { Heading } from '@/components/heading';
 import { Spinner } from '@/components/spinner';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import { ChoreCreateForm } from '@/features/chores/components/chore-create-form';
 import { ChoreList } from '@/features/chores/components/chore-list';
 
@@ -17,15 +11,12 @@ export default function ChoresPage() {
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Chores Page" description="All your chores here" />
 
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>Create Chore</CardTitle>
-          <CardDescription>A new chore will be created</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ChoreCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title="Create Chore"
+        description="A new chore will be created"
+        content={<ChoreCreateForm />}
+        className="w-full max-w-[420px] self-center"
+      />
 
       <Suspense fallback={<Spinner />}>
         <ChoreList />
