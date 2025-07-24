56ad6df32e7637ae8672136673fbea5007845d4f
ChrisKnowsDev <chrisharley81@gmail.com>
2025-07-24 05:26:22 -0400

    create creact-chore form placeholder

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -2,12 +2,28 @@ import { Suspense } from 'react';
 
 import { Heading } from '@/components/heading';
 import { Spinner } from '@/components/spinner';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { ChoreList } from '@/features/chores/components/chore-list';
 
 export default function ChoresPage() {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Chores Page" description="All your chores here" />
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Create Chore</CardTitle>
+          <CardDescription>A new chore will be created</CardDescription>
+        </CardHeader>
+        <CardContent>Chore create form</CardContent>
+      </Card>
+
       <Suspense fallback={<Spinner />}>
         <ChoreList />
       </Suspense>
