17c44cf113a4c8445bd21b7b0c89a17af7cd206e
ChrisKnowsDev <chrisharley81@gmail.com>
2025-07-25 14:33:40 -0400

    in chore-list add check if no chores found

--- src/features/chores/components/chore-list.tsx
+++ src/features/chores/components/chore-list.tsx
@@ -1,8 +1,14 @@
+import { notFound } from 'next/navigation';
+
 import { ChoreItem } from '@/features/chores/components/chore-item';
 import { getChores } from '@/features/chores/queries/get-chores';
 
 export async function ChoreList() {
   const chores = await getChores();
+
+  if (!chores) {
+    notFound();
+  }
 
   return (
     <div className="animate-fade-in-from-top flex flex-col items-center gap-y-8">
