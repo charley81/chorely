e0e43bf638f929151bf142ad9f88d1b050733bf9
ChrisKnowsDev <chrisharley81@gmail.com>
2025-08-07 04:57:47 -0400

    add link to chores from home

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 
 import { initialChores } from '@/data'
+import { chorePath } from '@/paths'
 
 export default function ChoresPage() {
   return (
     <div>
       {initialChores.map((chore) => (
-        <Link key={chore.id} href={`/chores/${chore.id}`}>
+        <Link key={chore.id} href={chorePath(chore.id)}>
           {chore.title}
         </Link>
       ))}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { choresPath } from '@/paths'
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Chorely</h1>
+  return (
+    <div>
+      <h1>Chorely</h1>
+      <Link href={choresPath()}>Go to chores</Link>
+    </div>
+  )
 }
