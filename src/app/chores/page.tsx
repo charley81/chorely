6dbc3c0f0e75d134433e7a6ec0ab23ac768deb54
ChrisKnowsDev <chrisharley81@gmail.com>
2025-08-12 12:29:20 -0400

    add client side data fetchting on ChoresPage

--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,13 +1,28 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 import { Heading } from '@/components/heading'
-import { initialChores } from '@/data'
 import ChoreList from '@/features/chores/components/chore-list'
+import { getChores } from '@/features/chores/queries/get-chores'
+import { Chore } from '@/features/chores/types'
 
 export default function ChoresPage() {
+  const [chores, setChores] = useState<Chore[]>([])
+
+  useEffect(() => {
+    const fetchChores = async () => {
+      const result = await getChores()
+      setChores(result)
+    }
+
+    fetchChores()
+  }, [])
   return (
     <>
       <Heading title="Chores" description="All your chores here" />
       <div className="animate-fade-in-from-top flex w-full flex-col items-center gap-y-4">
-        <ChoreList chores={initialChores} />
+        <ChoreList chores={chores} />
       </div>
     </>
   )
