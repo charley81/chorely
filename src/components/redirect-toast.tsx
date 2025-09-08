679f51f6431af1d1ae7a20c48ed1c2a13125ec53
ChrisKnowsDev <chrisharley81@gmail.com>
2025-09-08 19:54:23 -0400

    remove console logs

--- src/components/redirect-toast.tsx
+++ src/components/redirect-toast.tsx
@@ -6,17 +6,14 @@ import { toast } from 'sonner'
 import { deleteCookieByKey, getCookieByKey } from '@/actions/cookies'
 
 export function RedirectToast() {
-  console.log('redirect toast component is rendering')
   const pathname = usePathname()
 
   useEffect(() => {
     const showToastMessage = async () => {
-      console.log('checking for cookie on path', pathname)
       const message = await getCookieByKey('toast')
-      console.log('message from cookie', message)
 
       if (message) {
-        toast.success(message)
+        toast.success(message, { id: message })
         deleteCookieByKey('toast')
       }
     }
